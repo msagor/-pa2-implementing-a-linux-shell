bfccaaa6be42b4328e048110adf2ec70b03bd479
Mohammad Sagor <msagor@tamu.edu>
2021-09-24 20:29:59 -0500

    added includes

--- start.cpp
+++ start.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#include <iostream>
+#include<iostream>
+#include <vector>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 using namespace std;
 
 int main (){
